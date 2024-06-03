51801302408e2b3cf23fa2d085bf9c1db8f0e271
Mario <cinv.ocotal0201@uml.edu.ni>
2024-06-03 00:00:15 -0600

    Prueba de Commit

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,6 +5,8 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
+                <h3>Hola Soy Mario</h3>
+
                 <div class="card-header">{{ __('Dashboard') }}</div>
 
                 <div class="card-body">
